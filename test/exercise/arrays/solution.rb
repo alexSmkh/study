34a55228ecdbea4a2c949c8433e2beaaeaaf7273
Alexander Samokhvalov <alexshikari9333@gmail.com>
2021-11-12 20:20:08 +0300

    Fixed linter errors

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -6,17 +6,17 @@ module Exercise
         array.map { |el| el.positive? ? max_value : el }
       end
 
-      def search(_array, _query)
+      def search(array, query)
         no_found_value = -1
         left_index = 0
-        right_index = _array.length - 1
+        right_index = array.length - 1
 
         while right_index >= left_index
           middle_index = (right_index + left_index) / 2
-          middle_value = _array[middle_index]
-          return middle_index if middle_value == _query
+          middle_value = array[middle_index]
+          return middle_index if middle_value == query
 
-          if middle_value > _query
+          if middle_value > query
             right_index = middle_index - 1
           else
             left_index = middle_index + 1
